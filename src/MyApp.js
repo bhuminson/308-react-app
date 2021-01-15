7d83ed83a045673f8d0ff2df9a1b684258c577ff
bhuminson <bhumin0804@gmail.com>
2021-01-14 16:15:32 -0800

    App: update state only on 201 response

--- src/MyApp.js
+++ src/MyApp.js
@@ -26,7 +26,7 @@ function MyApp() {
   async function makePostCall(person) {
     try {
       const response = await axios.post("http://localhost:5000/users", person);
-      return response;
+      return response.status;
     } catch (error) {
       console.log(error);
       return false;
@@ -42,7 +42,7 @@ function MyApp() {
 
   function updateList(person) {
     makePostCall(person).then((result) => {
-      if (result) setCharacters([...characters, person]);
+      if (result == 201) setCharacters([...characters, person]);
     });
   }
 
